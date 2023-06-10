3ba81ba7718b1cead31d97522769c73edc734313
harshitaM29 <harshita.mundhe29@gmail.com>
2023-06-10 11:06:50 +0530

    Added input component

--- src/components/Meals/MealForm.js
+++ src/components/Meals/MealForm.js
@@ -1,11 +1,19 @@
 import classes from './MealForm.module.css';
+import Input from '../UI/Input';
 
 const MealForm = () => {
     return (
         <form className={classes.form}>
           <div>
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <Input 
+            label="Amount" input={{
+                id: "amount",
+                type: "number",
+                min:"1",
+                max:"5",
+                step:"1",
+                defaultValue: "1"
+            }}/>
         </div>
             <button type="submit">+Add</button>
             </form>
